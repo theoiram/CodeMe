087d041bc6ca022a050e19bdf4677283d8eca8af
Manuel Serrato <mars_tfm@hotmail.com>
2017-07-19 19:22:45 -0500

    Input maxlength on Auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,7 +28,7 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" maxlength="50" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -42,7 +42,7 @@
                             <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" maxlength="30" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
