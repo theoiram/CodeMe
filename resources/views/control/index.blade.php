8bd70783a1cd625fb53123d686e5bfbf8bf48f45
willy6842 <30302712+willy6842@users.noreply.github.com>
2017-08-15 18:52:41 -0500

    Update index.blade.php
    ehhh

--- resources/views/control/index.blade.php
+++ resources/views/control/index.blade.php
@@ -44,12 +44,160 @@
             </tbody>
           </table>
         </div>
+      <!-- ///////////////////////////////////////////////////////////////////////////    -->
         <div id="tab2" class="tab-pane fade">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Id Transportista</th>
+                  <th>Nombre Transportista</th>
+                  <th>Correo</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>011</td>
+                  <td>Doe James</td>
+                  <td>james@example.com</td>
+                  <td>
+                    <button class="btn btn-primary">EDITAR</button>
+                    <button class="btn btn-danger">ELIMINAR</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>001</td>
+                  <td>Dooley</td>
+                  <td>july@example.com</td>
+                  <td>
+                    <button class="btn btn-primary">EDITAR</button>
+                    <button class="btn btn-danger">ELIMINAR</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
         </div>
+
+
+
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////   -->
         <div id="tab3" class="tab-pane fade">
+          <form class="form-horizontal">
+            <div class="row">
+              <div class="col-sm-10 col-sm-offset-1">
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="dropdown">
+                       <p>Tipo de Transportista</p>
+                      <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style="width: 100%;">Seleccionar
+                      <span class="caret"></span></button>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                      </ul>
+                    </div>
+                  </div>
+
+                <div class="dropdown col-sm-6">
+                   <p>Linea de Transporte</p>
+                  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style="width: 100%;">Seleccionar
+                  <span class="caret"></span></button>
+                  <ul class="dropdown-menu" style="    width: 100%; text-align: center;">
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                  </ul>
+                </div>
+
+                </div>
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="col-sm-6 dropdown" style="padding: 0">
+                       <p>Placa camion</p>
+                      <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style="width: 100%;">Seleccionar
+                      <span class="caret"></span></button>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                      </ul>
+                    </div>
+                    <div class="col-sm-6 dropdown" style="padding: 0">
+                       <p>Numero de caja</p>
+                      <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style="width: 100%;">Seleccionar
+                      <span class="caret"></span></button>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="form-group" style="margin-right: 0; margin-left: 0;">
+                      <p>Nombre responsable</p>
+                        <input class="form-control" id="disabledInput" type="text" placeholder="..." disabled>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="dropdown">
+                       <p>Tipo de Transporte</p>
+                      <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style="width: 100%;">Seleccionar
+                      <span class="caret"></span></button>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div class="col-sm-6">
+                    <div class="col-sm-6">
+                      <p>Fecha y hora salida</p>
+                      <input type="text" name="date" value="10/24/1994" />
+                    </div>
+
+                    <div class="col-sm-6">
+                      <p>Fecha y hora salida</p>
+                      <input type="text" name="date" value="10/24/1994" />
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="form-group col-sm-12">
+                    <label for="comment">Comentarios:</label>
+                    <textarea class="form-control" rows="5" id="comment"></textarea>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </form>
         </div>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////   -->
         <div id="tab4" class="tab-pane fade">
         </div>
+      </div>
+    </div>
+
+
+    <script type="text/javascript">
+    $(function() {
+        $('input[name="birthdate"]').daterangepicker({
+            singleDatePicker: true,
+            showDropdowns: true
+        },
+        function(start, end, label) {
+            var years = moment().diff(start, 'years');
+            alert("You are " + years + " years old.");
+        });
+    });
+    </script>
       </div>
     </div>
 @endsection
